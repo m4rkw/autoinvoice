4cf1fefdbeced35cad1c14ba94e5d695292dee3e
Mark Wadham <m@rkw.me>
2011-09-17 01:41:16 +0100

    Dependencies unit test class.

---
+++ tests/unit/Dependencies_Test.php
@@ -0,0 +1,31 @@
+<?
+class DependenciesTest extends PHPUnit_Framework_TestCase {
+	function test_construct_loads_zend_loader() {
+		$this->assertTrue(class_exists('Zend_Loader'));
+	} 
+
+	function test_dependency_zend_loader() {
+		include_once('Zend/Loader.php');
+	}
+
+	function test_dependency_zend_gdata() {
+		Zend_Loader::loadClass('Zend_Gdata');
+	}
+
+	function test_dependency_zend_gdata_authsub() {
+		Zend_Loader::loadClass('Zend_Gdata_AuthSub');
+	}
+
+	function test_dependency_zend_gdata_clientlogin() {
+		Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+	}
+
+	function test_dependency_zend_gdata_httpclient() {
+		Zend_Loader::loadClass('Zend_Gdata_HttpClient');
+	}
+
+	function test_dependency_zend_gdata_calendar() {
+		Zend_Loader::loadClass('Zend_Gdata_Calendar');
+	}
+}
+?>
